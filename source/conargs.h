dda5b3b0d0c9e22c88705bc19d51b93f85659692
sage-etcher <sage.codes@email.com>
2023-12-21 14:23:39 -0500

    added licensing comments to source files

--- source/conargs.h
+++ source/conargs.h
@@ -1,3 +1,35 @@
+/* 
+ * This source file is a part of the Leaf Package Manager.
+ *
+ * /source/conargs.h
+ * Contains prototypes and includes for code defined in '/source/conargs.c'.
+ *
+ */
+
+/* 
+ * Copyright 2023 Sage I. Hendricks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* 
+ * email:  sage.message@email.com
+ * github: sage-etcher
+ *
+ */
+
+
 /* run once */
 #pragma once
 #ifndef __PKG_CONARGS_HEADER__
